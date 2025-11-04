503d4d27605f32c286f95f92837747b0f450df87
Mickey Vershbow <mickey.vershbow@gmail.com>
2025-11-03 21:10:28 -0800

    set up scene, camera, renderer

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+
+// 3 things renderer, camera, scene object
+
+// set size of renderer
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+// set up the renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+
+// append to canvas element the DOM
+document.body.appendChild(renderer.domElement);
+
+// set up the camera
+const fov = 75; //75 degrees
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2; // scootch the camera back a little
+
+// set up the scene
+const scene = new THREE.Scene();
+
+// render the scene
+renderer.render(scene, camera)
